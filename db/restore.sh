43f410492542a283720728bf8b4e36c2d7c97d22
Filipe Paternot <fpaternot@corp.globo.com>
2018-04-25 14:54:26 -0300

    [db/restore] Improved debug messages

--- db/restore.sh
+++ db/restore.sh
@@ -2,9 +2,10 @@
 
 
 DBHOST="$1"
-EXTRADIR='/mnt/projetos/deploy-be/zabbix_keys/db'
+EXTRADIR='/opt/zabbix_keys/db'
 EXTRAFILE="${EXTRADIR}/${DBHOST}"
 DBNAME="$2"
+TMPDIR="/dev/shm"
 usage() {
   echo "import.sh DBHOST DBNAME"
   echo "note: DBHOST must be a filename inside ${EXTRADIR}"
@@ -24,18 +25,23 @@ path="/opt/zabbix_backup/zabbix/master.database.suaempresa.com/";
 i=`ls $path|tail -n1`;
 echo "Found $i. Coping to curr dir..";
 sql="zbx-conf-bkup-${i}.sql";
-cd /dev/shm;
+cd $TMPDIR;
 cp ${path}/${i}/${sql}.gz .;
 [ ! -f "${sql}" ] && echo "Ungziping.." && gzip -d ${sql}.gz;
 if [ -f "${sql}" ]; then
   echo "Cleaning target database"
   DEL=`echo "select '/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;' union select concat('drop table ',table_schema,'.',table_name,';') from information_schema.tables where table_schema = '$DBNAME' union select '/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;'" | mysql --defaults-extra-file=${EXTRAFILE};`;
   time mysql --defaults-extra-file=${EXTRAFILE} ${DBNAME} -e "$DEL";
-  echo "Importing to mysql database ${db}..";
+  echo "Importing to mysql database ${DBNAME} (${sql})..";
   time mysql --defaults-extra-file=${EXTRAFILE} ${DBNAME} < ${sql};
   echo "Removing partitions";
   time mysql --defaults-extra-file=${EXTRAFILE} ${DBNAME} -e "ALTER TABLE \`history\` REMOVE PARTITIONING; ALTER TABLE \`history_log\` REMOVE PARTITIONING; ALTER TABLE \`history_str\` REMOVE PARTITIONING; ALTER TABLE \`history_text\` REMOVE PARTITIONING; ALTER TABLE \`history_uint\` REMOVE PARTITIONING; ALTER TABLE \`trends\` REMOVE PARTITIONING; ALTER TABLE \`trends_uint\` REMOVE PARTITIONING;"
   rm -f ${sql};
+else
+  echo "Failed to find ${sql}";
+  cd $OLDPWD;
+  date;
+  exit 1;
 fi
 echo "Finished..";
 cd $OLDPWD;
