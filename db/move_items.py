17c2e36c443249071bac93ab1b3c7b2037209be8
Augusto da Silva Costa <gutoscosta@gmail.com>
2018-10-08 22:52:20 -0300

    Add License to *.py files

--- db/move_items.py
+++ db/move_items.py
@@ -1,5 +1,11 @@
 #!/bin/env python
 #from __future__ import print_function
+"""
+Copyright (c) 2018.
+This file is part of globocom/zabbix-scripts
+(see https://github.com/globocom/zabbix-scripts).
+License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
+"""
 import pymysql,sys,os,json
 from pyzabbix import ZabbixAPI
 from argparse import ArgumentParser
