7fc558239b0542efd98a14bb02835894a1dccec1
Augusto da Silva Costa <gutoscosta@gmail.com>
2018-10-11 20:28:07 -0300

    Update restore.sh

--- db/restore.sh
+++ db/restore.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-#Copyright (c) 2018.
-#This file is part of globocom/zabbix-scripts
-#(see https://github.com/globocom/zabbix-scripts).
-#License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2016, Globo.com <https://github.com/globocom>
+# This file is part of globocom/zabbix-scripts
+# (see https://github.com/globocom/zabbix-scripts).
+# License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
 
 
 DBHOST="$1"
